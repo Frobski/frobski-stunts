271be286f4c3356894431b82624ed3e3dd30d059
Frobski <37448330+Frobski@users.noreply.github.com>
2021-07-23 19:34:46 +0200

    Initial push

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.KMH = true ------ If set to false it uses mph
+Config.KMHspeed = 40 ------- how many kmh you need to stunt
+Config.MPHspeed = 20 ------- how many mph you need to stunt requires Config.KMH off
+
+Config.Bikes = {
+    'sanchez01',
+    'hakuchou2'
+}
