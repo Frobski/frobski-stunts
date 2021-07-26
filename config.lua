30da2abd8975bafc427139ae4db7ae88caf50369
Frobski <37448330+Frobski@users.noreply.github.com>
2021-07-26 05:10:39 +0200

    Small fix to allow custom bikes

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.KMHspeed = 40 ------- how many kmh you need to stunt
 Config.MPHspeed = 20 ------- how many mph you need to stunt requires Config.KMH off
 
 Config.Bikes = {
-    'sanchez01',
-    'hakuchou2'
+    '2015crf450',
+    'SANCHEZ01',
+    'HAKUCHOU2'
 }
